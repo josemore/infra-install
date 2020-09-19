65cdce89385f0eb44429243f3cf14948ce51a6ea
Frederick Ryckbosch <fryckbosch@newrelic.com>
2020-09-19 21:02:04 +0200

    Add all supported Ubuntu versions.

--- install.sh
+++ install.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 set -e
 
-curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
-echo "license_key: $NR_LICENSE_KEY" | sudo tee /etc/newrelic-infra.yml
-printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt bionic main" | sudo tee /etc/apt/sources.list.d/newrelic-infra.list
-sudo apt-get update
-sudo apt-get install newrelic-infra -y
+function unsupported {
+    echo "Unsupported distro."
+}
+
+function install_ubuntu {
+    RELEASE=$1
+    curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+    echo "license_key: $NR_LICENSE_KEY" | sudo tee /etc/newrelic-infra.yml
+    echo "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt $RELEASE main" | sudo tee /etc/apt/sources.list.d/newrelic-infra.list
+    sudo apt-get update
+    sudo apt-get install newrelic-infra -y
+}
+
+DISTRO=$(cat /etc/issue | grep -Eo "(Ubuntu)" 2>/dev/null)
+
+if [ "$DISTRO" == "Ubuntu" ]; then
+    RELEASE=$(lsb_release -sc)
+
+    if [[ $RELEASE == "focal" ]]; then
+        $RELEASE="bionic"
+    fi
+
+    if [[ $RELEASE =~ (bionic|xenial|trusty|precise) ]]; then
+        install_ubuntu $RELEASE
+    else
+        unsupported && exit 1
+    fi
+else
+    unsupported && exit 1
+fi
