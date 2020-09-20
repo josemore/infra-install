c6ab2a71d931bf3ebf0d4a2d908a52cdde563b83
Frederick Ryckbosch <fryckbosch@newrelic.com>
2020-09-20 10:04:01 +0200

    Add CentOS support.

--- install.sh
+++ install.sh
@@ -3,26 +3,35 @@ set -e
 
 function unsupported {
     echo "Unsupported distro."
+    exit 1
 }
 
 function install_ubuntu {
-    RELEASE=$1
-    curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
-    echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml
-    echo "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt $RELEASE main" | sudo tee /etc/apt/sources.list.d/newrelic-infra.list
-    sudo apt-get update
-    sudo apt-get install newrelic-infra -y
+    CODENAME=$1
+    if [[ $CODENAME =~ (bionic|xenial|trusty|precise) ]]; then
+        curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+        echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml
+        echo "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt $CODENAME main" | sudo tee /etc/apt/sources.list.d/newrelic-infra.list
+        sudo apt-get update
+        sudo apt-get install newrelic-infra -y
+    else
+        unsupported
+    fi
 }
 
-function install_amazon {
+function install_redhat {
     RH_RELEASE=$1
-    echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml &&
-    sudo curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/$RH_RELEASE/x86_64/newrelic-infra.repo
-    sudo yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
-    sudo yum install newrelic-infra -y
+    if [[ $RH_RELEASE =~ ^(5|6|7|8)$ ]]; then
+        echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml &&
+        sudo curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/$RH_RELEASE/x86_64/newrelic-infra.repo
+        sudo yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
+        sudo yum install newrelic-infra -y
+    else
+        unsupported
+    fi
 }
 
-DISTRO=$(cat /etc/issue /etc/system-release | grep -m 1 -Eo "(Ubuntu|Amazon)" 2>/dev/null)
+DISTRO=$(cat /etc/issue /etc/system-release /etc/redhat-release | grep -m 1 -Eo "(Ubuntu|Amazon|CentOS)" 2>/dev/null)
 
 if [ "$DISTRO" == "Ubuntu" ]; then
     RELEASE=$(lsb_release -sc)
@@ -31,17 +40,19 @@ if [ "$DISTRO" == "Ubuntu" ]; then
         RELEASE="bionic"
     fi
 
-    if [[ $RELEASE =~ (bionic|xenial|trusty|precise) ]]; then
-        install_ubuntu $RELEASE
-    else
-        unsupported && exit 1
-    fi
+    install_ubuntu $RELEASE
+
 elif [ "$DISTRO" == "Amazon" ]; then
     if [[ $(cat /etc/system-release) =~ " release 2 " ]]; then
-        install_amazon 7
+        install_redhat 7
     else
-        install_amazon 6
+        install_redhat 6
     fi
+
+elif [ "$DISTRO" == "CentOS" ]; then
+    source /etc/os-release
+    install_redhat $REDHAT_SUPPORT_PRODUCT_VERSION
+
 else
-    unsupported && exit 1
+    unsupported
 fi
--- test/variables.tf
+++ test/variables.tf
@@ -33,17 +33,17 @@ variable "product_tag" {
 variable "distros" {
   description = "Distributions to test install (co-indexed with amis and ami_users)"
   type        = list(string)
-  default     = ["ubuntu20", "ubuntu18", "ubuntu16", "ubuntu14", "ubuntu12", "amazon2", "amazon"]
+  default     = ["ubuntu20", "ubuntu18", "ubuntu16", "ubuntu14", "ubuntu12", "amazon2", "amazon", "centos8", "centos7", "centos6"]
 }
 
 variable "amis" {
   description = "AMIs for the distributions to test install"
   type        = list(string)
-  default     = ["ami-0383535ce92966dfe", "ami-0c32816f296ee28e5", "ami-04bfff099c4b1d8ed", "ami-0a273e2936ffb0ab9", "ami-ee0b0688", "ami-08a2aed6e0a6f9c7d", "ami-0a7c31280fbd23a86"]
+  default     = ["ami-0383535ce92966dfe", "ami-0c32816f296ee28e5", "ami-04bfff099c4b1d8ed", "ami-0a273e2936ffb0ab9", "ami-ee0b0688", "ami-08a2aed6e0a6f9c7d", "ami-0a7c31280fbd23a86", "ami-0bfa4fefe067b7946", "ami-0d4002a13019b7703", "ami-a625b8df"]
 }
 
 variable "ami_users" {
   description = "Users for the distributions to test install"
   type        = list(string)
-  default     = ["ubuntu", "ubuntu", "ubuntu", "ubuntu", "ubuntu", "ec2-user", "ec2-user"]
+  default     = ["ubuntu", "ubuntu", "ubuntu", "ubuntu", "ubuntu", "ec2-user", "ec2-user", "centos", "centos", "centos"]
 }
