0244990fbeb7c5679642e3d8a8919628829f7a3d
Frederick Ryckbosch <fryckbosch@newrelic.com>
2020-09-27 11:30:34 +0200

    Updated README, LICENSE. Improved logging.

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
-Easy way to install New Relic Infra agent
-
 ![End-to-end test](https://github.com/fryckbos/infra-install/workflows/End-to-end%20test/badge.svg)
+
+# Install New Relic Infra agent on Linux
+
+This repository contains a script (`install.sh`) that provides an easy way to install the New Relic Infra agent on the following Linux distributions:
+
+ - **Amazon Linux** (2, 1)
+ - **Debian** (10, 9, 8)
+ - **CentOS** (8, 7, 6)
+ - **Red Hat Enterprise Linux** (8, 7, 6)
+ - **SUSE Linux Enterprise Server** (15, 12, 11)
+ - **Ubuntu** (20.04, 18.04, 16.04, 14.04, 12.04)
+
+# Usage
+
+Get your license key from the New Relic UI and execute the script with the license key in the `NR_LICENSE_KEY` environment variable:
+
+```NR_LICENSE_KEY={{LICENSE KEY}} bash -c "$(curl -L https://raw.githubusercontent.com/fryckbos/infra-install/master/install.sh)"```
+
+# Contributing
+
+This repo is open to contributions, such as adding a new Linux distribution or version:
+
+ - Make your changes
+ - Update the tests to cover potential new distros
+ - Run the tests to ensure they pass
+ - Create a PR to this repo
+
+# License
+
+This project is licensed under the Apache 2 License.
+
+# End-to-end tests
+
+The end-to-end test (`test-all.sh`) uses **Terraform** to deploy an EC2 instance for each of the supported Linux distributions. The following steps are executed:
+ - terraform init
+ - terraform apply
+ - terraform destroy
+ - processing of the results
+
+During the `terraform apply` phase, the `test/test.sh` script is copied to the EC2 instances and the following steps are executed:
+ - setup required testing dependencies
+ - setup hosts file and run netcat to capture data going to New Relic on localhost
+ - curl-into-bash installation using the `install.sh` script
+ - wait until we capture network data for New Relic (3 min timeout)
+
+## Adding a new distribution
+
+The Terraform variables file (`test/variables.tf`) contains the tested Linux distributions. There are 3 co-indexed lists: `distros`, `amis` and `ami_users`.
+
+In order to add a distribution, add the name of the distribution to the `distros` list, the ami-id to the `amis` list and the username to ssh into the EC2 instance to the `ami_users` list. 
+
+Make sure to add the name, ami-id and username to the same position in the list (for example the last position).
+
+## Running the tests locally
+
+Prerequisites
+ - An AWS account
+ - Setup the AWS configuration and credentials file as described [here](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html).
+ - SSH keypair in `~/.ssh/id_rsa.pub` and `~/.ssh/id_rsa`
+
+After running the `test-all.sh` script, the output of the terraform apply can be found in `test/output`. There you can find the output of all `test.sh` script runs prefixed with the instance.
+
+To debug the tests, you can run `test-all.sh --keep` to keep the EC2 instances. You can login to the instances by looking up the IP address using `cd test && terraform show`. Make sure to use the username specified in the variables to login (eg. `ssh centos@1.2.3.4` for CentOS boxes).
+
+Use `cd test && terraform destroy` to destroy the instances.
--- install.sh
+++ install.sh
@@ -1,25 +1,77 @@
 #!/bin/bash
 set -e
 
+function info {
+    echo -e "\033[1;33m$1\033[0m"
+}
+
 function unsupported {
-    echo "Unsupported distro."
+    echo -e "\033[1;31mThis Linux distribution is not supported by this script.\033[0m"
+    echo
+    echo "The supported distros are:"
+    echo "  - Amazon Linux (2, 1)"
+    echo "  - Debian (10, 9, 8)"
+    echo "  - CentOS (8, 7, 6)"
+    echo "  - Red Hat Enterprise Linux (8, 7, 6)"
+    echo "  - SUSE Linux Enterprise Server (15, 12, 11)"
+    echo "  - Ubuntu (20.04, 18.04, 16.04, 14.04, 12.04)"
+    echo
+    echo "Create a GitHub issue (https://github.com/fryckbos/infra-install/issues) to request support."
     exit 1
 }
+
+function success {
+    echo
+    echo -e "\033[1;32mSuccessfully installed the New Relic Infra agent!\033[0m"
+    echo
+    echo "For more information about the agent check out our docs at https://docs.newrelic.com/docs/infrastructure/install-infrastructure-agent/get-started/install-infrastructure-agent"
+}
+
+function error {
+    echo
+    echo -e "\033[1;31mSomething went wrong during the installation of the agent.\033[0m"
+    echo
+    echo "For more information see the documentation at"
+    echo
+    echo "   https://docs.newrelic.com/docs/infrastructure/install-infrastructure-agent/get-started/install-infrastructure-agent"
+    echo
+    echo "If that doesn't solve the issue, please contact support@newrelic.com and provide your distribution, version and the output of the script."
+}
+
+trap error ERR
 
 function install_apt {
     CODENAME=$1
     if [[ $CODENAME =~ (bionic|xenial|trusty|precise|buster|stretch) ]]; then
+        info "Creating the config file with your license key"
         echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml
+
+        info "Setting up the New Relic Infrastructure Agent gpg key"
         dpkg -s gnupg >/dev/null 2>/dev/null || sudo apt-get update && sudo apt-get install -y gnupg
         curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+
+        info "Adding the New Relic Infrastructure Agent apt repo"
         echo "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt $CODENAME main" | sudo tee /etc/apt/sources.list.d/newrelic-infra.list
+
+        info "Updating the apt cache"
         sudo apt-get update
+
+        info "Installing the agent"
         sudo apt-get install newrelic-infra -y
     elif [ "$CODENAME" == "jessie" ]; then
+        info "Creating the config file with your license key"
         echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml
+
+        info "Setting up the New Relic Infrastructure Agent gpg key"
         curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+
+        info "Adding the New Relic Infrastructure Agent apt repo"
         echo "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt $CODENAME main" | sudo tee /etc/apt/sources.list.d/newrelic-infra.list
+
+        info "Updating the apt cache"
         sudo apt-get -o Acquire::Check-Valid-Until=false update || echo "Expected failure because of deprecation."
+
+        info "Installing the agent"
         sudo apt-get install newrelic-infra -y
     else
         unsupported
@@ -29,8 +81,13 @@ function install_apt {
 function install_redhat {
     RH_RELEASE=$1
     if [[ $RH_RELEASE =~ ^(5|6|7|8)$ ]]; then
+        info "Creating the config file with your license key"
         echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml
+
+        info "Adding the New Relic Infrastructure Agent yum repo"
         sudo curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/$RH_RELEASE/x86_64/newrelic-infra.repo
+
+        info "Installing the agent"
         sudo yum -y --disablerepo='*' --enablerepo='newrelic-infra' install newrelic-infra
     else
         unsupported
@@ -40,23 +97,41 @@ function install_redhat {
 function install_suse {
     SUSE_RELEASE=$1
     if [ $SUSE_RELEASE = "11.4" ]; then
+        info "Creating the config file with your license key"
         echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml
+
+        info "Setting up the New Relic Infrastructure Agent gpg key"
         curl -s -o /tmp/newrelic-infra.gpg https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg && sudo rpm --import /tmp/newrelic-infra.gpg
+
+        info "Adding the New Relic Infrastructure Agent zypper repo"
         sudo curl -o /etc/zypp/repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/zypp/sles/11.4/x86_64/newrelic-infra.repo
+
+        info "Updating the zypper cache"
         sudo zypper -n ref -r newrelic-infra
+
+        info "Installing the agent"
         sudo zypper -n install newrelic-infra
     elif [ $SUSE_RELEASE = "12.4" ]; then
+        info "Creating the config file with your license key"
         echo "license_key: $NR_LICENSE_KEY" | sudo tee -a /etc/newrelic-infra.yml
+
+        info "Setting up the New Relic Infrastructure Agent gpg key"
         curl -s -o /tmp/newrelic-infra.gpg https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg && sudo rpm --import /tmp/newrelic-infra.gpg
+
+        info "Adding the New Relic Infrastructure Agent zypper repo"
         sudo curl -o /etc/zypp/repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/zypp/sles/12.4/x86_64/newrelic-infra.repo
+
+        info "Updating the zypper cache"
         sudo zypper -n ref -r newrelic-infra
+
+        info "Installing the agent"
         sudo zypper -n install newrelic-infra
     else
         unsupported
     fi
 }
 
-DISTRO=$(cat /etc/issue /etc/system-release /etc/redhat-release /etc/os-release 2>/dev/null | grep -m 1 -Eo "(Ubuntu|Amazon|CentOS|Debian|Red Hat|SUSE)")
+DISTRO=$(cat /etc/issue /etc/system-release /etc/redhat-release /etc/os-release 2>/dev/null | grep -m 1 -Eo "(Ubuntu|Amazon|CentOS|Debian|Red Hat|SUSE)" || true)
 
 if [ "$DISTRO" == "Ubuntu" ] || [ "$DISTRO" == "Debian" ]; then
     RELEASE=$(lsb_release -sc)
@@ -92,3 +167,5 @@ elif [ "$DISTRO" == "SUSE" ]; then
 else
     unsupported
 fi
+
+success
